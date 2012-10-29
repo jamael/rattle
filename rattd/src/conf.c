ad04007dc0e0d0df19224cacc7e52007f92cfb0c
Jamael Seun <jamael.seun@gmail.com>
2012-10-29 14:45:23 -0400

    fix invalid read if src is an empty char

--- rattd/src/conf.c
+++ rattd/src/conf.c
@@ -138,22 +138,24 @@ static int set_value(void *dst, void const * const src, int type, int flags)
 	}
 
 	/*
-	 * We check for numeric boundaries first even if source value
-	 * type is not known yet -- three reasons I see:
+	 * We check for numeric boundaries first
+	 * -- two reasons I see:
 	 *
-	 * 1. check_num() knows about value type so only numbers may fail
-	 *    the check.  That and invalid value type.
-	 *
-	 * 2. Saves the work of a _get_tail_next() if a list is requested
+	 * 1. Saves the work of a _get_tail_next() if a list is requested
 	 *    and the numeric value is rejected soon after.
 	 *
-	 * 3. This makes the code easier to read than multiple
+	 * 2. This makes the code easier to read than multiple
 	 *    check_num() in the switch()-case below.
 	 */
-	retval = check_num(type, *((int *)src), (flags & RATTCONFFLUNS));
-	if (retval != OK) {
-		debug("check_num() failed");
-		return FAIL;
+	if ((type == RATTCONFDTNUM8)
+	    || (type == RATTCONFDTNUM16)
+	    || (type == RATTCONFDTNUM32)) {
+		retval = check_num(type, *((int *) src),
+		    (flags & RATTCONFFLUNS));
+		if (retval != OK) {
+			debug("check_num() failed");
+			return FAIL;
+		}
 	}
 
 	if (flags & RATTCONFFLLST) {
